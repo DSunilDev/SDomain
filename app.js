7cadb90f610743f29330d823df15ae3f005a5a84
DSunilDev <sunilkumardeveops@gmail.com>
2023-11-29 13:40:30 +0530

    D D

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ app.use(express.urlencoded({extended:true}))
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
+app.use(express.static('dicons'));
 app.use(express.static('css'));
 app.use(express.static('fonts'));
 app.use(express.static('img'));
 app.use(express.static('imgs'));
 app.use(express.static('js'));
 app.use(express.static('mail'));
-app.use(express.static('dicons'));
+
 
 
 app.get('/',function(req,res)
